91fb2bd38b141518e963a33714cf6f71a2a48ea1
xps13 <huqinghua109@163.com>
2018-09-08 23:22:14 +0800

    mod excel path

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
+import os
+
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
 UPLOAD_FOLDER = './tmp/uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'doc'])
-EXCEL_BASIS = "E:\\Desktop\\fhzbWeb\\report\\cleandata_basis.xlsx"
-EXCEL_CORNDATA = "E:\\Desktop\\fhzbWeb\\report\\CornData.xlsx"
+EXCEL_BASIS = os.path.join(os.path.dirname(os.path.abspath(__file__)),'report/cleandata_basis.xlsx')
+EXCEL_CORNDATA = os.path.join(os.path.dirname(os.path.abspath(__file__)),'report/CornData.xlsx')
