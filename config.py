5753c563e5a1585334840d4fc44ecf7cbdf3d6ee
Dean <huqinghua109@163.com>
2019-05-15 16:29:06 +0800

    mod webdata

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'doc'])
 
 EXCEL_TEMPRES = os.path.join(os.path.dirname(os.path.abspath(__file__)),'report/CornTempRes.xlsx')
 
-EXCEL_WEBDATA = os.path.join(os.path.dirname(os.path.abspath(__file__)),'report/WebData.xlsx')
+EXCEL_WEBDATA = os.path.join(os.path.dirname(os.path.abspath(__file__)),'report/webData.xlsx')
 EXCEL_CORNPRICE = os.path.join(os.path.dirname(os.path.abspath(__file__)),'report/CornPrice.xlsx')
 EXCEL_DEEPPROCESS = os.path.join(os.path.dirname(os.path.abspath(__file__)),'report/DeepProcessOprationAndProfit.xlsx')
